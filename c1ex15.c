d8dc28bdceab0d11330eb3b2c15d63bbd008f6d6
WallaceMariano <wallacewvinfo@gmail.com>
2023-01-03 20:40:37 -0300

    First commit

---
+++ c1ex15.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    printf("Bem-vindos ao /Mundo\\ da programação em \"C\"\n");
+
+    return 0;
+}
---
+++ c1ex17.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    printf("Total\t-\t100%%\n");
+    printf("IVA\t-\t17%%\n");
+    printf("IRS\t-\t15%%\n");
+    printf("--------------------\n");
+    printf("Liq.\t-\t68%%\n");
+
+    return 0;
+}
