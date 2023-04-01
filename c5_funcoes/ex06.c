150c68fd7a6d81e0806778c5d436a6f9565abc07
WallaceMariano <wallacewvinfo@gmail.com>
2023-04-01 17:21:16 -0300

    Add ex05 in chapter 5

---
+++ c5_funcoes/ex06.c
@@ -0,0 +1,9 @@
+// long int n_segundos(int n_horas)
+// Devolve o número de segundos existentes em um conjunto de horas.
+
+#include <stdio.h>
+
+long int n_segundos(int n_horas)
+{
+    return n_horas != 0: (n_horas *= 3600): n_horas;
+}
